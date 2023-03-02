9d1a6ad15fa048e0d3f085fd061c7a01def3454d
Kodigos <105288094+Kodacod@users.noreply.github.com>
2023-03-02 09:50:10 -0300

    Add files via upload

---
+++ Primeiro_Projeto/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exercício</title>
+</head>
+<body>
+    <h1><?php echo "Ola Mundo" ?></h1>
+
+    <h2>5 + 3 = <?php echo(5+3)?></h2>
+    <h2>7 - 2 = <?php echo(7-2)?></h2>
+    <h2>8 * 4 = <?php echo(8*4)?></h2>
+    <h2>10 / 5 = <?php echo(10/5)?></h2>
+
+</body>
+</html>
